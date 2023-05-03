ad9ed58a5c28883f420daedf352885483e497c84
holdmydata <codymcox@gmail.com>
2023-05-02 21:28:00 -0500

    tasks for eval chatbot/ placeholder

---
+++ GPT/chatbot.py
@@ -0,0 +1,10 @@
+## Coming Soon! Chatbot for my model so that I can test it locally and possibly running it on a server.
+## To Do:
+## - Add a backend memory for the model to store information.
+## - Add a UI for the chatbot.
+## - Add a UI for the model training and evaluation.
+## - Implement with good ol' YupBot!
+
+
+
+
